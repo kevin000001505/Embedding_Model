9792cbeaa466a1c275f15daa9142154e28c60083
Cheng-Hsun, Hsu <a10121555@gmail.com>
2025-09-14 18:31:22 -0400

    latest version. reconstruct main.py

--- main.py
+++ main.py
@@ -3,34 +3,39 @@ import re
 import json
 import random
 import emoji
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Union
 from bs4 import BeautifulSoup as bs
 import torch
 import torch.nn as nn
 import pandas as pd
 
 
+def read_file(file_path: str) -> str:
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def list_all_files(folder_path: str) -> List[str]:
+    files = []
+    for root, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
+
+
 class DataProcessing:
-    """Clean the data and build vocabulary."""
+    """Clean the tweet data and create a new folder cleaned_tweet with cleaned data"""
 
-    def __init__(
-        self, train_dir: str = "./tweet/train", test_dir: str = "./tweet/test"
-    ):
-        self.train_dir = train_dir
-        self.test_dir = test_dir
-        self.training = pd.DataFrame(columns=["content", "label"])
-        self.testing = pd.DataFrame(columns=["content", "label"])
-
-    def list_all_files(self, folder_path: str) -> List[str]:
-        files = []
-        for root, _, filenames in os.walk(folder_path):
-            for filename in filenames:
-                files.append(os.path.join(root, filename))
-        return files
-
-    def read_file(self, file_path: str) -> str:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
+    def __init__(self, input_dir: str = "./tweet", output_dir: str = "./cleaned_tweet"):
+        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(os.path.join(output_dir, "test", "negative"), exist_ok=True)
+        os.makedirs(os.path.join(output_dir, "test", "positive"), exist_ok=True)
+        os.makedirs(os.path.join(output_dir, "train", "negative"), exist_ok=True)
+        os.makedirs(os.path.join(output_dir, "train", "positive"), exist_ok=True)
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        self.pos_vocab = set()
+        self.neg_vocab = set()
 
     def remain_capital_words(self, content: str) -> str:
         # If the words have more than one capital letter, keep it as is; otherwise, convert to lowercase
@@ -59,90 +64,64 @@ class DataProcessing:
 
         return content.strip()
 
-    def build_vocabulary(self, label: int) -> set:
-        """
-        Build vocabulary from texts with specified label. And store the pandas DataFrame as json file.
-        """
+    def save_cleaned_file(self):
+        for subset in ["train", "test"]:
+            all_files = list_all_files(os.path.join(self.input_dir, subset))
+            for file_path in all_files:
+                content = self.data_cleaning(read_file(file_path))
+                label = os.path.basename(os.path.dirname(file_path))
 
-        filtered_data = self.training[self.training["label"] == label].copy()
+                if subset == "train" and content != "":
+                    tokens = content.split()
+                    if label == "positive":
+                        self.pos_vocab.update(tokens)
+                    else:
+                        self.neg_vocab.update(tokens)
 
-        # Store the tokens into column just to check the result in json file
-        filtered_data["tokens"] = filtered_data["content"].apply(lambda x: x.split())
-
-        filename = (
-            "positive_label_data.json" if label == 1 else "negative_label_data.json"
-        )
-
-        filtered_data.to_json(filename, orient="records", force_ascii=False, indent=2)
-        print(f"Successfully saved {filename}")
-
-        vocab = set()
-        for tokens in filtered_data["tokens"]:
-            vocab.update(tokens)
-
-        return vocab
-
-    def build_dataframe(self, file_dir: str) -> pd.DataFrame:
-        """Build a pandas DataFrame from files in the specified directory."""
-        file_paths = self.list_all_files(file_dir)
-        rows = []
-        for path in file_paths:
-            rows.append(
-                {
-                    "content": self.read_file(path),
-                    "label": (
-                        1
-                        if os.path.basename(os.path.dirname(path)) == "positive"
-                        else 0
-                    ),
-                }
-            )
-        return pd.DataFrame(rows)
-
-    def main(self) -> Dict[str, any]:
-
-        self.training = self.build_dataframe(self.train_dir)
-
-        self.training["content"] = self.training["content"].apply(self.data_cleaning)
-        self.training = self.training[self.training["content"] != ""]
-
-        positive_vocab = self.build_vocabulary(label=1)
-        word2idx = {word: idx for idx, word in enumerate(positive_vocab)}
-        idx2word = {idx: word for word, idx in word2idx.items()}
-
-        result = {
-            "positive_vocab": list(positive_vocab),
-            "word2idx": word2idx,
-            "idx2word": idx2word,
-        }
-
+                relpath = os.path.relpath(file_path, self.input_dir)
+                output_path = os.path.join(self.output_dir, relpath)
+                with open(output_path, "w", encoding="utf-8") as f:
+                    f.write(content)
+        pos_word2idx = {word: idx for idx, word in enumerate(self.pos_vocab)}
+        neg_word2idx = {word: idx for idx, word in enumerate(self.neg_vocab)}
         with open("vocab.json", "w", encoding="utf-8") as f:
-            json.dump(result, f, indent=2, ensure_ascii=False)
+            json.dump(
+                {
+                    "positive": {
+                        "vocabulary": list(self.pos_vocab),
+                        "word2idx": pos_word2idx,
+                        "idx2word": {idx: word for word, idx in pos_word2idx.items()},
+                    },
+                    "negative": {
+                        "vocabulary": list(self.neg_vocab),
+                        "word2idx": neg_word2idx,
+                        "idx2word": {idx: word for word, idx in neg_word2idx.items()},
+                    },
+                },
+                f,
+                ensure_ascii=False,
+                indent=2,
+            )
 
-        print("Successfully saved vocab.json")
-        return result
 
-
-class PrepareTrainingData:
-    """Prepare the training data for the Neural Network and logistic regression model."""
+class PrepareData:
+    """Prepare the data for the Neural Network and logistic regression model."""
 
     def __init__(
-        self, vocab: Dict[str, any], file_dir: str = "positive_label_data.json"
+        self,
+        vocab_path: str = "vocab.json",
     ):
-        self.word2idx = vocab["word2idx"]
-        self.idx2word = vocab["idx2word"]
-        self.vocab = vocab["positive_vocab"]
-        self.training_data = pd.read_json(file_dir)
-        self.positive_samples = pd.DataFrame(columns=["training", "label"])
+        with open(vocab_path, "r", encoding="utf-8") as f:
+            self.vocab_dict = json.load(f)
 
     def generate_neg_samples(
-        self, tr_s: List[str], context_window: int
+        self, tr_s: List[str], context_window: int, label: str = "positive"
     ) -> Tuple[List[List[str]], List[int]]:
         """Generate negative samples for a positive samples. 2 * context_window negative samples."""
         neg_samples = []
 
         while len(neg_samples) < 2 * context_window:
-            negative_word = random.choice(self.vocab)
+            negative_word = random.choice(self.vocab_dict[label]["vocabulary"])
 
             if (
                 negative_word not in tr_s
@@ -152,8 +131,12 @@ class PrepareTrainingData:
 
         return (neg_samples, [0] * len(neg_samples))
 
-    def create_training_data(
-        self, data: List[str], context_window: int = 2, proximity: bool = False
+    def create_data(
+        self,
+        data: List[str],
+        context_window: int = 2,
+        proximity: bool = False,
+        label: str = "positive",
     ) -> Tuple[List[int], List[int]]:
         """Algorithm to generate the training data and labels."""
 
@@ -182,7 +165,7 @@ class PrepareTrainingData:
 
                         # generate negative samples (4*[target, negative_word], [0, 0, 0, 0])
                         generate_neg_samples = self.generate_neg_samples(
-                            sample, context_window
+                            sample, context_window, label
                         )
                         tr_s += generate_neg_samples[0]
                         lab_s += generate_neg_samples[1]
@@ -226,32 +209,72 @@ class PrepareTrainingData:
 
         except Exception as e:
             # For debugging purposes
-            print(f"Error in create_training_data at k={k}, i={i}: {e}")
+            print(f"Error in create_data at k={k}, i={i}: {e}")
             print(f"Data: {data}")
 
         return (tr_s, lab_s)
 
-    def main(self, context_window: int = 2, proximity: bool = False) -> pd.DataFrame:
+    def load_data(
+        self, label: str = "positive", file_dir: str = "train"
+    ) -> Dict[str, Union[List[str], Dict[str, int], Dict[int, str]]]:
+        """Load the cleaned data and vocabulary."""
+
+        word2idx = self.vocab_dict[label]["word2idx"]
+        idx2word = self.vocab_dict[label]["idx2word"]
+        vocab = self.vocab_dict[label]["vocabulary"]
+        contents = [
+            read_file(content)
+            for content in list_all_files(
+                os.path.join("./cleaned_tweet", file_dir, label)
+            )
+        ]
+        return {
+            "contents": contents,
+            "vocab": vocab,
+            "word2idx": word2idx,
+            "idx2word": idx2word,
+        }
+
+    def word_to_index(self, word: str, word2idx: Dict[str, int]) -> int:
+        """Convert a word to its corresponding index."""
+        # If the word is not found, return the index for <UNK> or -1 if <UNK> is also not found
+        return word2idx.get(word, word2idx.get("<UNK>", -1))
+
+    def main(
+        self,
+        context_window: int = 2,
+        proximity: bool = False,
+        label: str = "positive",
+        file_dir: str = "train",
+    ) -> pd.DataFrame:
+        """Main function to prepare the data."""
+
+        samples = pd.DataFrame(columns=["data", "label", "encoded_data"])
+
+        variables = self.load_data(label=label, file_dir=file_dir)
         all_tr = []
         all_lab = []
 
-        for _, row in self.training_data.iterrows():
-            tokens = row["content"].split()
+        for content in variables["contents"]:
+            tokens = content.split()
             try:
-                results = self.create_training_data(tokens, context_window, proximity)
+                results = self.create_data(tokens, context_window, proximity, label)
 
                 all_tr += results[0]
                 all_lab += results[1]
             except Exception as e:
-                print(f"Error processing row {_}: {e}")
-                print(f"Content: {row['content']}")
+                print(f"Error processing content: {e}")
+                print(f"Content: {content}")
                 print(f"Tokens: {tokens}")
 
-        self.positive_samples["training"] = all_tr
-        self.positive_samples["label"] = all_lab
-        print("Successfully created training samples.")
+        samples["data"] = all_tr
+        samples["label"] = all_lab
+        samples["encoded_data"] = samples["data"].apply(
+            lambda x: [self.word_to_index(word, variables["word2idx"]) for word in x]
+        )
+        print("Successfully created samples.")
 
-        return self.positive_samples
+        return samples
 
 
 class SimpleWord2Vec_LogiR(nn.Module):
@@ -306,39 +329,32 @@ class SimpleWord2Vec_FFNN(nn.Module):
 
 
 if __name__ == "__main__":
-    # If exist skip the data processing step
-    if not os.path.exists("positive_label_data.json") or not os.path.exists(
-        "vocab.json"
-    ):
-        processor = DataProcessing()
-        vocab = processor.main()
-    else:
+    if os.path.exists("./cleaned_tweet") and os.path.exists("./vocab.json"):
         print(
-            "positive_label_data.json and vocab.json already exists. Skipping data processing."
+            "cleaned_tweet folder already exists. Please remove it first if you want to re-generate."
         )
-        vocab = json.load(open("vocab.json", "r", encoding="utf-8"))
+    else:
+        processor = DataProcessing()
+        processor.save_cleaned_file()
+        print("Successfully cleaned the tweet data and saved in cleaned_tweet folder")
 
-    trainer = PrepareTrainingData(vocab)
-    word2index = vocab["word2idx"]
-    idx2word = vocab["idx2word"]
-
-    # We got DataFrame with 2 columns: "training": list object with target first and context next and "label": 0 or 1
-    training_data = trainer.main(context_window=2)
-
-    # we got [[idx_target, idx_context], ...]
-    training_data["idx"] = training_data["training"].apply(
-        lambda x: [word2index[word] for word in x]
+    dataloader = PrepareData(vocab_path="vocab.json")
+    training_data = dataloader.main(
+        context_window=2, proximity=False, label="positive", file_dir="train"
     )
-    x_train = torch.tensor(training_data["idx"].tolist())
+    testing_data = dataloader.main(
+        context_window=2, proximity=False, label="positive", file_dir="test"
+    )
+    vocab = dataloader.load_data(label="positive", file_dir="train")
+
+    x_train = torch.tensor(training_data["encoded_data"].tolist())
     y_train = torch.tensor(
         training_data["label"].tolist(), dtype=torch.float32
     ).unsqueeze(1)
 
-    log_model = SimpleWord2Vec_LogiR(
-        vocab_size=len(vocab["positive_vocab"]), embedding_dim=64
-    )
+    log_model = SimpleWord2Vec_LogiR(vocab_size=len(vocab["vocab"]), embedding_dim=64)
     nn_model = SimpleWord2Vec_FFNN(
-        vocab_size=len(vocab["positive_vocab"]), embedding_dim=64, node_size=32
+        vocab_size=len(vocab["vocab"]), embedding_dim=64, node_size=32
     )
     # Train for logistic regression
     lr = 0.01
